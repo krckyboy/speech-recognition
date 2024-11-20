ac54a737f0d16f7cb0ad20575eee10c56e483286
krckyboy <dusan.todorovic.2016@gmail.com>
2024-11-20 22:34:58 +0100

    Implement a very simple example of speech recognition API usage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [output, setOutput] = useState('');
+  const [buttonText, setButtonText] = useState('Start Voice Input');
+  const recognition = useRef(new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)());
+
+  const handleClick = () => {
+    recognition.current.start();
+  };
+
+  useEffect(() => {
+    recognition.current.lang = 'en-US';
+
+    recognition.current.onstart = () => {
+      setButtonText('Listening...');
+      setOutput('');
+    };
+
+    recognition.current.onresult = (event) => {
+      setOutput(event.results[0][0].transcript);
+    };
+
+    recognition.current.onend = () => {
+      setButtonText('Start Voice Input');
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className={'w-full flex justify-center items-center p-2 h-full bg-gray-800'}>
+      <div className="container mx-auto flex gap-6 flex-col justify-center items-center">
+        <p className={'text-gray-300 text-left w-full'}>{output}</p>
+        <button
+          onClick={handleClick}
+          className={'bg-cyan-700 text-gray-300 flex-1 flex-grow-0 flex items-center justify-center p-2 w-full'}
+        >
+          {buttonText}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
